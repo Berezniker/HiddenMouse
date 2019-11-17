e5773d8bb8d996283c4d8c15af7ef9b299de2255
Alexey <berezniker@mail.ru>
2019-11-17 18:52:42 +0300

    add some features; add database_preprocessing

--- feature_extraction/feature_extraction.py
+++ feature_extraction/feature_extraction.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import time as t
 import glob
 import os
 
@@ -43,12 +44,38 @@ def curve_length(db):
              (db.y.iloc[:-1].values - db.y.iloc[1:].values) ** 2) ** 0.5).sum()
 
 
+def curve_length_bin(db):
+    pass  # TODO
+
+
+def length_ratio(db):
+    return curve_length(db) / (actual_distance(db) + 1e-5)
+
+
+def actual_speed(db):
+    return actual_distance(db) / (db.time.iloc[-1] - db.time.iloc[0])
+
+
+def curve_speed(db):
+    return ((((db.x.iloc[:-1].values - db.x.iloc[1:].values) ** 2 +
+              (db.y.iloc[:-1].values - db.y.iloc[1:].values) ** 2) ** 0.5)
+            / (db.time.iloc[1:].values - db.time.iloc[:-1].values)
+            ).mean()
+
+
+def curve_acceleration(db):
+    return curve_speed(db) / (db.time.iloc[-1] - db.time.iloc[0])
+
+
 def database_preprocessing(db):
     db.rename({'client timestamp': 'time'}, axis=1, inplace=True)
     db.drop('record timestamp', axis=1, inplace=True)
     db.drop_duplicates(inplace=True)
+    db.drop_duplicates(subset='time', inplace=True)
+    # db.loc[db.time.duplicated(), 'time'] += 5e-4
 
 
+# TODO need to speed up
 def split_dataframe(db, time_threshold=1.0):
     time, max_time = 0, db['time'].max()
     while time + time_threshold < max_time:
@@ -59,16 +86,18 @@ def split_dataframe(db, time_threshold=1.0):
 
 def extract_features(database):
     database_preprocessing(database)
+    # don't forget to add your function here:
+    extraction_function = [actual_distance, curve_length, length_ratio,
+                           actual_speed, curve_speed, curve_acceleration]
     features = dict()
     for segment in split_dataframe(database):
-        features.setdefault('actual_distance', []).append(actual_distance(segment))
-        features.setdefault('curve_length', []).append(curve_length(segment))
-        # TODO
+        for extractor in extraction_function:
+            features.setdefault(extractor.__name__, []).append(extractor(segment))
         break
     return features
 
 
-def extractor(mode):
+def run(mode):
     data_path = f'../dataset/{mode}_files'
     save_path = f'../features/{mode}_features'
 
@@ -89,5 +118,9 @@ def extractor(mode):
 
 
 if __name__ == '__main__':
-    extractor('train')
-    # extractor('test')
+    start_time = t.time()
+    run('train')
+    print('train_time:', t.time() - start_time)
+    # start_time = t.time()
+    # run('test')
+    # print('train_time:', t.time() - start_time)
